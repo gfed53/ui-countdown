86c572e8d99b5fc699f30c4efc57a08a47922a7e
gfed53 <gfederico7153@gmail.com>
2017-06-28 12:52:36 -0400

    Added bg asset, gulp-imagemin dependency, basic styling with bg, updated code to clear interval once we reach end time

--- js/script.js
+++ js/script.js
@@ -4,23 +4,25 @@ $(function(){
 	
 	// .format('MMMM Do YYYY, h:mm:ss a')
 	
-	var end = moment('2017-12-25','YYYY-MM-DD');
+	let end = moment('2017-12-25','YYYY-MM-DD');
 
 	console.log(end);
 
 	function init(){
-		var timeInterval = setInterval(()=>{
-			var t = calcTimeRemaining(end);
-
-
-			$('.test').html(`
+		let timeInterval = setInterval(()=>{
+			let t = calcTimeRemaining(end);
+			$('.countdown').html(`
 				${t.days} days. ${t.hours} hours. ${t.minutes} minutes. ${t.seconds} seconds..
 				`);
+			
+			if(t.total <= 0){
+				clearInterval(timeInterval);
+			}
 		}, 1000);
 	}
 
 	function calcTimeRemaining(endTime){
-		var diff = endTime.diff(moment(), true),
+		let diff = endTime.diff(moment(), true),
 			diffDays = Math.floor((diff/(1000*60*60*24))),
 			diffHours = Math.floor((diff/(1000*60*60)) % 24),
 			diffMins = Math.floor((diff/1000/60) % 60),
