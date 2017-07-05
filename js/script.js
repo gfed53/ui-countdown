f1e754a684b1633ac5b5e930e9f8b9e60214e845
gfed53 <gfederico7153@gmail.com>
2017-07-05 13:04:57 -0400

    Adjusted blur duration. Code cleanup

--- js/script.js
+++ js/script.js
@@ -2,31 +2,24 @@
 
 $(function(){
 	
-	// .format('MMMM Do YYYY, h:mm:ss a')
-	let $main = $('#main');
-	
-	let end = moment('2017-12-25','YYYY-MM-DD');
-
-	console.log(end);
-
-	let durations = [2000,3000,4000,5000,6000,7000];
+	let $main = $('#main'),
+		end = moment('2017-12-25','YYYY-MM-DD'),
+		durations = [2000,3000,4000,5000,6000,7000],
+		isLooping = false;
 
 	function init(){
 		let timeInterval = setInterval(()=>{
 			let t = calcTimeRemaining(end);
-			// $('.countdown').html(`
-			// 	<span class="cd-days">${t.days} days.</span> <span>${t.hours} hours.</span> <span>${t.minutes} minutes.</span> <span>${t.seconds} seconds..</span>
-			// 	`);
 
 			$('.countdown span:nth-child(1)').html(`${t.days} days.`);
 			$('.countdown span:nth-child(2)').html(`${t.hours} hours.`);
 			$('.countdown span:nth-child(3)').html(`${t.minutes} minutes.`);
 			$('.countdown span:nth-child(4)').html(`${t.seconds} seconds.`);
 
-			
 			if(t.total <= 0){
 				clearInterval(timeInterval);
 			}
+
 		}, 1000);
 
 		display();
@@ -51,42 +44,21 @@ $(function(){
 	}
 
 	function display(){
-		//Refactor into loop?
-		$('.countdown span:nth-child(1)').velocity({
-			opacity: 1
-		},{
-			duration: 50,
-			delay: 1000
-		});
-		$('.countdown span:nth-child(2)').velocity({
-			opacity: 1
-		},{
-			duration: 50,
-			delay: 2000
-		});
-		$('.countdown span:nth-child(3)').velocity({
-			opacity: 1
-		},{
-			duration: 50,
-			delay: 3000
-		});
-		$('.countdown span:nth-child(4)').velocity({
-			opacity: 1
-		},{
-			duration: 50,
-			delay: 4000
-		});
+		for(let i = 1; i<5; i++){
+			$('.countdown span:nth-child('+i+')').velocity({
+				opacity: 1
+			},{
+				duration: 50,
+				delay: 1000*i
+			});
+		}
 	}
 
 	//Create interval that blurs and sharpens periodically
 
-	let isLooping = false;
-
 	function blurLoop(){
-		console.log('blurLoop start');
 		let interval = setInterval(()=>{
 			if(!isLooping){
-				console.log('blurLoop inner execution');
 				let length = durations[Math.floor(Math.random()*durations.length)];
 				blurRound(length);
 			}
@@ -94,10 +66,8 @@ $(function(){
 	}
 
 	function blurRound(length){
-		console.log('blurRound start. length:',length);
 		isLooping = true;
 		let blurOuter = setTimeout(()=>{
-			console.log('blurOuter timeout end');
 			blurEffect(()=>{
 				clearTimeout(blurOuter);
 				isLooping = false;
@@ -106,19 +76,16 @@ $(function(){
 	}
 
 	function blurEffect(cb){
-		console.log('blurEffect start');
 		$main.addClass('blur');
 		let blurInner = setTimeout(()=>{
 			$main.removeClass('blur');
 			clearTimeout(blurInner);
 			cb();
-		}, 1000);
+		}, 500);
 		
 	}
 
 	init();
-
-	console.log(typeof diff);
 
 
 });
